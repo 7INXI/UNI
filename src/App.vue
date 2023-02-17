89feab527592859047ff19bd5797267e987f5005
7INXIAL <7INXIAL@gmail.com>
2023-02-17 08:56:01 +0800

    ts: 测试

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
-// import { onLaunch, onShow, onHide } from "@dcloudio/uni-app";
-// onLaunch(() => {});
-// onShow(() => {});
-// onHide(() => {});
+import { onLaunch, onShow, onHide } from "@dcloudio/uni-app";
+onLaunch(() => {});
+onShow(() => {});
+onHide(() => {});
 </script>
 <style></style>
